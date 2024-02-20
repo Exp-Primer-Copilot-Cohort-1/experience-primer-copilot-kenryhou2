528b1bc27b7f8018a1b4cdb6ba22bf8e2901c468
Henry Kou <kouhenry@yahoo.com>
2024-02-20 22:16:45 +0000

    git altering comments.js

--- comments.js
+++ comments.js
@@ -1,28 +1,48 @@
 //create a web server
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const comments = require('./comments.json');
-
-app.use(bodyParser.json());
-
-//serve up the comments page
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-//create a comment
-app.post('/comments', (req, res) => {
-  const { name, comment } = req.body;
-  if (name && comment) {
-    comments.push({ name, comment });
-    res.json({ name, comment });
-  } else {
-    res.status(400).json({ message: 'Name and comment are required.' });
-  }
-});
-
-//start server
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var querystring = require('querystring');
+//create a server object:
+http.createServer(function (req, res) {
+    //parse the url
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    if (req.method === 'GET') {
+        if (filename === './comments') {
+            //read the file
+            fs.readFile('comments.txt', function (err, data) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            });
+        } else {
+            //read the file
+            fs.readFile(filename, function (err, data) {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            });
+        }
+    } else if (req.method === 'POST') {
+        if (filename === './comments') {
+            var body = '';
+            req.on('data', function (data) {
+                body += data;
+            });
+            req.on('end', function () {
+                var post = querystring.parse(body);
+                fs.appendFile('comments.txt', post['comment'] + '\n', function (err) {
+                    if (err) throw err;
+                    console.log('Saved!');
+                });
+                res.writeHead(302, { 'Location': '/comments' });
+                res.end();
+            });
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end("404 Not Found");
+        }
+    }
+}).listen(8080); //the server object listens on port 8080
+console.log('Server running at http://
